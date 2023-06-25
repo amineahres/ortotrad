0974a1642b8754dc971f9932f3b780afddf47fdc
Amine <amine.ahres@gmail.com>
2023-06-25 13:15:51 +0100

    Update app.py

--- api/app.py
+++ api/app.py
@@ -17,28 +17,22 @@ def index():
     if request.method == 'POST':
 
         # Retrieve all users inputs
-        input_type = request.form['input_type']
-        input_tone = request.form['input_tone']
-        input_length = request.form['input_length']
-        input_context = request.form['input_context']
-        input_content = request.form['input_content']
-
+        input_instructions = request.form['input_instructions']
 
         # Make sure that content is not null
-        if input_content is None or len(input_content) < 10:
-            chatgpt_result = "Please fill in the content with at least 10 characters."
+        if instructions is None or len(input_content) < 5:
+            chatgpt_result = "Merci de renseigner au moins 5 lettres"
             return render_template('index.html', chatgpt_response=chatgpt_result)
         else:
             # Create prompt
-            prompt = ('Find between percent signs an input. from that input, please create a ' + input_type + 
-            ' which has the following characteristics: \n' +
-            '- Tone: ' + input_tone + ' \n' +
-            '- Length: ' + input_length + ' \n' +
-            'and take into account the following request: ' + input_context + ' \n' +
-            '% \n' +
-            input_content + 
-            '\n%')
-            
+           prompt = ('As the assistant to an orthodontist specializing in clear aligners, your expertise is pivotal in conveying treatment plans to the design team. The orthodontist provides detailed instructions in his own language enclosed within percent signs. \n'+
+           'Your assignment is to meticulously translate these instructions into English, ensuring the accurate usage of medical terminologies. \n'+        
+           'Only the translated text is required in the response \n' +
+           '% \n' +
+           input_instructions + 
+           '\n%')
+
+
             # Use OpenAI's API to get the response from ChatGPT
             headers = {
                 'Content-Type': 'application/json',
@@ -56,7 +50,7 @@ def index():
                 json={
                     "messages": messages,
                     "model": "gpt-3.5-turbo",
-                    "max_tokens": 600,
+                    #"max_tokens": 600,
                     "temperature": 0.5
                 }
             )
@@ -64,20 +58,16 @@ def index():
             # Handle the ChatGPT response and extract the adapted query
             chatgpt_result = None
     
-            
             if chatgpt_response.status_code == 200:
                 chatgpt_result = chatgpt_response.json()
                 chatgpt_result = chatgpt_result["choices"][0]["message"]["content"]
-            
-            
+                    
             else:
                 print(chatgpt_response.status_code)
                 print(chatgpt_response.content)
                 chatgpt_result = "Error: Failed to receive response from ChatGPT"
                 
             #return render_template('index.html', chatgpt_response=chatgpt_result)
-
-            print('level 1 reached')
             
             url: str = os.environ.get("SUPABASE_URL")
             key: str = os.environ.get("SUPABASE_KEY")
